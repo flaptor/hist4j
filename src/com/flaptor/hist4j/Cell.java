ffbaab7d9f4a8c47a22b7a4ac954de9b2c2c8e64
jhandl <jhandl@c6592eba-9841-0410-bc39-6dc2fe70b7f7>
2008-03-12 01:00:55 +0000

    added serializable to the Cell class.

--- src/com/flaptor/hist4j/Cell.java
+++ src/com/flaptor/hist4j/Cell.java
@@ -3,11 +3,14 @@
  */
 package com.flaptor.hist4j;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
 /**
  * Contains the data of a histogram node.
  * @author jorge
  */
-public class Cell {
+public class Cell implements Serializable {
     public long count;
     public float minValue, maxValue;
 }
